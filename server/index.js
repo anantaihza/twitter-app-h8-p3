85a8da2b8f0c54719e7722bdc46a16e59afc7699
Ananta Ihza <anantaihza21@gmail.com>
2024-08-19 11:01:30 +0700

    feat: setup apollo from documentation

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const { ApolloServer }  = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries 
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+})
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at: ${url}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
