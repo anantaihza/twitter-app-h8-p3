fafa9ab0f39973180bc2a272c0d2d1b538370986
Ananta Ihza <anantaihza21@gmail.com>
2024-08-21 16:22:27 +0700

    feat: add template Register

---
+++ client/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import Login from './screens/Login';
+import Register from './screens/Register';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.containerSafeArea}>
+        <StatusBar style="auto" />
+        <Register />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerSafeArea: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
---
+++ client/screens/Register.js
@@ -0,0 +1,122 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableHighlight,
+} from 'react-native';
+
+export default function Register() {
+  const handleRegister = () => {};
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.containerLogin}>
+        <Image
+          source={require('../assets/logo-twitter.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.heading}>Register</Text>
+        <Text style={styles.subHeading}>Welcome to the new twitter</Text>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            placeholderTextColor={'#4C9EEB'}
+            // onChangeText={onChangeText}
+            // value={text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor={'#4C9EEB'}
+            // onChangeText={onChangeText}
+            // value={text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor={'#4C9EEB'}
+            // onChangeText={onChangeText}
+            // value={text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor={'#4C9EEB'}
+            // onChangeText={onChangeText}
+            // value={text}
+          />
+
+          <TouchableHighlight onPress={handleRegister} style={styles.btnSolid}>
+            <Text style={styles.btnTextSolid}>Register</Text>
+          </TouchableHighlight>
+        </View>
+
+        <TouchableHighlight onPress={handleRegister} style={styles.btnOutline}>
+          <Text style={styles.btnTextOutline}>Create new Account</Text>
+        </TouchableHighlight>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerLogin: {
+    flex: 1,
+    paddingHorizontal: 30,
+    backgroundColor: '#FFFFFF',
+    position: "relative"
+  },
+  logo: {
+    marginHorizontal: 'auto',
+    marginVertical: 100,
+  },
+  heading: {
+    fontWeight: '700',
+    fontSize: 35,
+    color: '#0C0C0C',
+  },
+  subHeading: {
+    color: '#9D9D9D',
+  },
+  input: {
+    height: 54,
+    marginTop: 12,
+    borderWidth: 1,
+    borderColor: '#4C9EEB',
+    paddingHorizontal: 20,
+    borderRadius: 50,
+  },
+  btnSolid: {
+    marginTop: 30,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    backgroundColor: '#4C9EEB',
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  btnOutline: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderWidth: 1,
+    borderColor: '#4C9EEB',
+    borderRadius: 50,
+    alignItems: 'center',
+    position: "relative",
+    bottom: -100,
+  },
+  btnTextSolid: {
+    color: '#FFFFFF',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  btnTextOutline: {
+    color: '#4C9EEB',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
